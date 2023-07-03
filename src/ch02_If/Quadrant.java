e820eb0382b694d5105665e19bcee83050416e32
yoodeve <janhold@naver.com>
2023-07-04 08:48:35 +0900

    백준 챕터2 조건문 - 사분면

---
+++ src/ch02_If/Quadrant.java
@@ -0,0 +1,27 @@
+package ch02_If;
+
+import java.util.Scanner;
+
+public class Quadrant {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		double thousand = 1000;
+
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+
+		if (-thousand <= x && x <= thousand && x != 0 && -thousand <= y && y <= thousand && y != 0) {
+			if (x > 0 && y > 0) {
+				System.out.println(1);
+			} else if (x < 0 && y > 0) {
+				System.out.println(2);
+			} else if (x < 0 && y < 0) {
+				System.out.println(3);
+			} else if (x > 0 && y < 0) {
+				System.out.println(4);
+			}
+
+		}
+	}
+}
